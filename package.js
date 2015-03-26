80a59ecc928a482337ad37dc2dac27fa02973081
Rahul <rchoudhury@gmail.com>
2015-03-25 22:01:04 -0700

    Remove stylus dependency
    Also update for autopublish support

--- package.js
+++ package.js
@@ -1,13 +1,12 @@
 Package.describe({
   name: 'rahul:animate-everything',
   summary: "Patch the hot code reload CSS live injection feature to transition all CSS changes.",
-  version: "1.0.1",
-  git: "https://github.com/Primigenus/animate-everything",
+  version: "1.0.2",
+  git: "https://github.com/Primigenus/animate-everything.git",
   debugOnly: true
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use('stylus', 'client');
-  api.addFiles('rahul-animate-everything.styl');
+  api.addFiles('rahul-animate-everything.css');
 });
